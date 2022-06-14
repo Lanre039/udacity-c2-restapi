0bf6999abc78f80b89107716e56e4591aaeabb5c
Lanre039 <rajilat1000@gmail.com>
2022-06-14 14:49:20 +0100

    restructure folder

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import { IndexRouter } from "./controllers/v0/index.router";
 
 (async () => {
   // Init the Express application
@@ -13,32 +13,11 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   app.use(bodyParser.json());
 
   // Root URI call
+  app.use("/api/v0/", IndexRouter);
+
+  // Root URI call
   app.get("/", async (req, res) => {
-    res.send("Looks good");
-  });
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  app.get("/filteredimage", async (req: Request, res: Response) => {
-    let { image_url } = req.query;
-    // 1
-    if (!image_url) {
-      return res.status(400).send(`Image Url is required`);
-    }
-
-    // 2. call filterImageFromURL(image_url) to filter the image
-    try {
-      const filePath = await filterImageFromURL(image_url as string);
-
-      // 4. deletes any files on the server on finish of the response
-      setTimeout(() => {
-        deleteLocalFiles([filePath]);
-      }, 2000);
-
-      // 3. send the resulting file in the response
-      return res.status(200).sendFile(filePath);
-    } catch (error) {
-      return res.status(400).send(error);
-    }
+    res.send("/api/v0/");
   });
 
   // Start the Server
